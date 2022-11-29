98945b5ea82045528d745e9fa6a864e2bb2dc16a
MatyasKloub <92371073+MatyasKloub@users.noreply.github.com>
2022-11-29 10:04:03 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "mbed.h"
+#include "LCD_DISCO_F469NI.h"
+#include "makra.h"
+
+
+LCD_DISCO_F469NI lcd;
+
+Thread thread1, thread2;
+
+
+
+void text1_thread()
+{   
+        lcd.SetTextColor(BARVA1);
+        lcd.DisplayStringAtLine(SMER1, (uint8_t *)TEXT1);
+        ThisThread::sleep_for(1s);
+}
+
+void text2_thread()
+{   
+
+    lcd.SetTextColor(BARVA2);
+    lcd.DisplayStringAtLine(SMER2, (uint8_t *)TEXT2);
+    ThisThread::sleep_for(1s);
+}
+
+
+
+int main()
+{    
+    thread1.start(text1_thread());
+    thread1.join();
+    lcd.Clear(BARVA3);
+    ThisThread::sleep_for(1s);
+
+    thread2.start(text2_thread());
+    thread2.join();
+    lcd.Clear(BARVA3);
+    ThisThread::sleep_for(2s);
+
+
+}
+
+
---
+++ makra.h
@@ -0,0 +1,14 @@
+#include <string>
+#include "LCD_DISCO_F469NI.h"
+
+// makra.h
+
+#define SMER1 1
+#define SMER2 2
+#define SMER3 3
+
+
+#define BARVA1 LCD_COLOR_BLUE
+#define BARVA2 LCD_COLOR_ORANGE
+#define BARVA3 LCD_COLOR_WHITE
+
